4aebc55267567343fba4e638085a0609ea1d17b1
Jonathan Michel <jonathan.michel@adesso.de>
2019-11-12 14:08:13 +0100

    Improved Fashion Gan (lr-decay, model storing, bigger models)

--- FashionGAN.py
+++ FashionGAN.py
@@ -3,13 +3,14 @@ from tensorflow import keras
 import numpy as np
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
+import datetime
 import time
 
 # globals:
 cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
 GEN_LR = 5e-5
-DISC_LR = 5e-5
-EPOCHS = 50
+DISC_LR = 3e-5
+EPOCHS = 150
 BATCH_SIZE = 256
 BUFFER_SIZE = 60000
 NOISE_DIMS = 100
@@ -31,7 +32,7 @@ def make_generator_model():
     model.add(layers.LeakyReLU())
 
     model.add(layers.Reshape((7, 7, 256)))
-    assert model.output_shape == (None, 7, 7, 256)  # Note: None is the batch size
+    assert model.output_shape == (None, 7, 7, 256)
 
     model.add(layers.Conv2DTranspose(128, (5, 5), strides=(1, 1), padding='same', use_bias=False))
     assert model.output_shape == (None, 7, 7, 128)
@@ -86,7 +87,17 @@ def discriminator_loss(real_output, fake_output):
     return real_loss + fake_loss
 
 
-def generate_image(generator, seed):
+def generator_accuracy(fake_output):
+    return tf.math.reduce_sum(tf.where(fake_output > 0.5, 1, 0)) / fake_output.shape[0]
+
+
+def discriminator_accuracy(real_output, fake_output):
+    negatives = tf.math.reduce_sum(tf.where(fake_output > 0.5, 0, 1))
+    positives = tf.math.reduce_sum(tf.where(real_output > 0.5, 1, 0))
+    return (negatives + positives) / (real_output.shape[0] + fake_output.shape[0])
+
+
+def generate_image(generator):
 
     predictions = generator(noise, training=False)
 
@@ -101,11 +112,13 @@ def generate_image(generator, seed):
 
 
 if __name__ == '__main__':
+
+    log_dir = "logs/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    file_writer = tf.summary.create_file_writer(log_dir + "/metrics")
+    graph_writer = tf.summary.create_file_writer(log_dir + "/graphs")
+
     fashion_mnist = keras.datasets.fashion_mnist
     (train_images, _), (_, _) = fashion_mnist.load_data()
-
-    plt.imshow(train_images[1000], cmap='gray')
-    plt.show()
 
     train_images = train_images.reshape(train_images.shape[0], 28, 28, 1).astype('float32')
     train_images = (train_images - 127.5) / 127.5
@@ -115,18 +128,25 @@ if __name__ == '__main__':
     generator = make_generator_model()
     discriminator = make_discriminator_model()
 
-    print(generator.summary())
-
-    gen_optimizer = tf.keras.optimizers.Adam(GEN_LR)
-    disc_optimizer = tf.keras.optimizers.Adam(DISC_LR)
-
 
     @tf.function
-    def train_step(images):
-        noise = tf.random.normal([BATCH_SIZE, NOISE_DIMS])
+    def train_step(images, epoch):
+
+        noise_ = tf.random.normal([BATCH_SIZE, NOISE_DIMS])
+
+        if epoch == 15:
+            GEN_LR = 2e-5
+            DISC_LR = 1e-5
+
+        if epoch == 50:
+            GEN_LR = 8e-6
+            DISC_LR = 8e-6
+
+        gen_optimizer = tf.keras.optimizers.Adam(GEN_LR)
+        disc_optimizer = tf.keras.optimizers.Adam(DISC_LR)
 
         with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
-            generated_images = generator(noise, training=True)
+            generated_images = generator(noise_, training=True)
 
             real_output = discriminator(images, training=True)
             fake_output = discriminator(generated_images, training=True)
@@ -138,21 +158,38 @@ if __name__ == '__main__':
         gradients_of_discriminator = disc_tape.gradient(disc_loss, discriminator.trainable_variables)
 
         gen_optimizer.apply_gradients(zip(gradients_of_generator, generator.trainable_variables))
-        disc_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+
+        if np.random.random() > 0.1:
+            disc_optimizer.apply_gradients(zip(gradients_of_discriminator, discriminator.trainable_variables))
+        else:
+            gradients_of_generator = 0
+
+        with file_writer.as_default():
+            tf.summary.scalar("generator_accuracy", generator_accuracy(fake_output), step=gen_optimizer.iterations)
+            tf.summary.scalar("discriminator_accuracy", discriminator_accuracy(real_output, fake_output),
+                              step=gen_optimizer.iterations)
+            tf.summary.scalar("generator_loss", gen_loss, step=gen_optimizer.iterations)
+            tf.summary.scalar("discriminator_loss", disc_loss, step=gen_optimizer.iterations)
+
 
     def train(dataset, epochs):
-        generate_image(generator, SEED)
+        generate_image(generator)
         for epoch in range(epochs):
             start = time.time()
 
             for batch in dataset:
-                train_step(batch)
+
+                train_step(batch, epoch)
 
             print("Epoch {} finished. Training time: {}".format(epoch, time.time() - start))
-            generate_image(generator, SEED)
+            generate_image(generator)
 
-        generate_image(generator, SEED)
+        generate_image(generator)
 
+
+    tf.summary.trace_on(graph=True)
     train(dataset, EPOCHS)
-    generator.save("generator.h5")
-    discriminator.save("discriminator.h5")
+    with graph_writer.as_default():
+        tf.summary.trace_export(name="graph", step=0)
+    generator.save("models\FashionGenerator.h5")
+    discriminator.save("models\FashionDiscriminator.h5")
